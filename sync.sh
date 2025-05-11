771bfcf90b38b062ff192f035a1e298e07a4bbe8
Jesse-Cameron <jessec.cameron@gmail.com>
2025-05-11 16:48:14 +1000

    feat: add sync command

---
+++ sync.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+stow -v p10k
+stow -v tmux
+stow -v aliases
+stow -v --adopt git
+if [ ! -d "$HOME"/.scripts ]; then
+  mkdir "$HOME"/.scripts;
+fi
+stow -v --target="$HOME"/.scripts scripts  
+stow -v --adopt zsh
